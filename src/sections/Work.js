0223d1382e9938dd64feb60bd6a1c0fd8d0fa6a0
Jolene <43065205+Jolene986@users.noreply.github.com>
2020-08-03 10:27:41 +0200

    Wp data removed

--- src/sections/Work.js
+++ src/sections/Work.js
@@ -2,15 +2,15 @@ import React from 'react'
 import SingleWork from '../components/SingleWork'
 //import data1
 import {projects} from '../data'
-import {wpProjects} from '../data'
 
-//import data2
+
+
 const Work = () => {
   return (
     <section id="work" className="work">
       <div className="projects">
 {projects.map(item => <SingleWork key={item.name} name={item.name} project={item.project} image={item.image} code={item.code}/>)}
-        {/* map data 1 into singlework */}
+        {/* map data  into singlework */}
       </div>
       
     </section>
